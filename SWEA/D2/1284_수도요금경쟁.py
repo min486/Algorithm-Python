1a4975ed95a3a987142da709568bc79e7b8eb0e3
min <smh92333@gmail.com>
2022-08-26 23:07:10 +0900

    0826 D2 풀이

---
+++ SWEA/D2/1284_수도요금경쟁.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    P, Q, R, S, W = map(int, input().split())
+    a_fee = W * P
+
+    if W <= R:
+        b_fee = Q
+    else: 
+        b_fee = Q + (W - R) * S
+
+    print(f'#{tc} {min(a_fee, b_fee)}')

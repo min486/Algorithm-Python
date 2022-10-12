fca82f1fe8a3fa97dab0ca657c4677f0dfe2215b
min <smh92333@gmail.com>
2022-10-12 22:11:07 +0900

    1012 5번째 풀이

---
+++ 백준/코드와트_스터디/2839_설탕배달.py
@@ -0,0 +1,17 @@
+gram = int(input())
+min_cnt = 0
+
+while gram > 0:
+    if gram % 5 == 0:  # 5의 배수면 몫 추가
+        min_cnt += (gram // 5)
+        print(min_cnt)
+        break
+    gram -= 3  # 5의 배수 아니면 3kg 감소, 개수 1 추가
+    min_cnt += 1
+
+    if gram == 0:  # 3의 배수였으면 0이됨
+        print(min_cnt)
+        break
+    
+else:
+    print(-1)

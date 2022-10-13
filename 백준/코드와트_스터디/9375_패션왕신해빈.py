042d03031b581627d159fcc8ae99f469d44df992
min <smh92333@gmail.com>
2022-10-13 21:05:44 +0900

    1013 5번째 풀이

---
+++ 백준/코드와트_스터디/9375_패션왕신해빈.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    dic = {}
+
+    for _ in range(n):
+        dress = list(input().split())
+        # 의상 종류별로 딕셔너리에 저장
+        if dress[1] in dic:
+            dic[dress[1]] += 1
+        else:
+            dic[dress[1]] = 2  # 입는 경우 + 안입는 경우
+    
+    result = 1
+    for cnt in dic.values():
+        result *= cnt  # 모든 경우의 수
+
+    print(result - 1)  # 알몸인 경우의 수 1 빼기

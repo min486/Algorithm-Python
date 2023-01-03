d00616305b9586d9ac6bf1d3297110e4ae4200e1
min <smh92333@gmail.com>
2023-01-03 23:49:06 +0900

    230103 D1 복습

--- SWEA/D1/2072_홀수만더하기.py
+++ SWEA/D1/2072_홀수만더하기.py
@@ -1,11 +1,11 @@
 T = int(input())
- 
-for tc in range(1, T + 1):
-    ten = list(map(int, input().split()))
-     
+
+for tc in range(1, T+1):
+    nums = list(map(int, input().split()))
+    
     result = 0
-    for i in ten:
+    for i in nums:
         if i % 2 == 1:
             result += i
-             
+            
     print(f'#{tc} {result}')

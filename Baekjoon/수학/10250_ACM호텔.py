fca36c52bc97d242e33f0917e70a525d9731804c
min <smh92333@gmail.com>
2023-04-08 22:14:22 +0900

    230408 백준 풀이

---
+++ Baekjoon/수학/10250_ACM호텔.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+for _ in range(T):
+    # h개의 층, w개의 방, n번째 손님
+    h, w, n = map(int, input().split())
+    result = 0
+
+    # 맨 왼쪽부터 아래에서 위로 배정
+    if n % h != 0:
+        # 방번호 = 층 * 100 + 호수
+        result = (n % h) * 100 + (n // h) + 1
+        
+    # n번째 손님이 가장 높은 층에 있을 때
+    else:
+        result = h * 100 + (n // h)
+
+    print(result)
---
+++ Baekjoon/수학/2884_알람시계.py
@@ -0,0 +1,11 @@
+h, m = map(int, input().split())
+
+if m < 45:
+    h -= 1
+    if h < 0:  # h가 0 => -1
+        h = 23
+    m = 60 - 45 + m  # m = m + 15의 값
+else :
+    m -= 45
+    
+print(h, m)

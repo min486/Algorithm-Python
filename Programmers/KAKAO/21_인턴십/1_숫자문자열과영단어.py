35bd738cd169be0ac45797ce4f3f682faa5e743f
min <smh92333@gmail.com>
2023-05-23 22:13:16 +0900

    230523 kakao 풀이

---
+++ Programmers/KAKAO/21_인턴십/1_숫자문자열과영단어.py
@@ -0,0 +1,22 @@
+# word = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+# def solution(s):
+#     result = ''
+#     leng = 0
+
+#     while leng < len(s):
+#         if '0' <= s[leng] <= '9':
+#             result += s[leng]
+#             leng += 1
+#         else:
+#             for i in range(10):
+#                 if s.find(word[i], leng) != -1:
+#                     result += str(i)
+#                     leng += len(word[i])
+#                     break
+
+#     answer = int(result)
+#     return answer
+
+test= 'abcd'
+print(test.find('cd', 2, 5))

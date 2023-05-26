e0939d7815aeb7cf99787bd63d6b554712264106
min <smh92333@gmail.com>
2023-05-26 23:22:40 +0900

    230526 kakao 풀이

--- Programmers/KAKAO/21_인턴십/2_거리두기확인하기.py
+++ Programmers/KAKAO/21_인턴십/2_거리두기확인하기.py
@@ -1,32 +1,31 @@
-import queue
+from collections import deque
 
-dr = ((-1,0), (1,0), (0,-1), (0,1))
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def bfs(place, row, col):
-    visited = [[False for _ in range(5)] for _ in range(5)]
-    q = queue.Queue()
-    visited[row][col] = True
-    q.put((row, col, 0))
+def bfs(place, x, y):
+    visited = [[False] * 5 for _ in range(5)]
+    q = deque()
+    q.append((x, y, 0))
+    visited[x][y] = True
 
-    while not q.empty():
-        curr = q.get()
-
-        if curr[2] > 2:
+    while q:
+        x, y, dist = q.popleft()
+        if dist == 2:
             continue
-        if curr[2] != 0 and place[curr[0]][curr[1]] == 'P':
-            return False
-        
         for i in range(4):
-            nr = curr[0] + dr[i][0]
-            nc = curr[1] + dr[i][1]
-            if nr < 0 or nr > 4 or nr < 0 or nc > 4:
-                continue
-            if visited[nr][nc]:
-                continue
-            if place[nr][nc] =='X':
-                continue
-            visited[nr][nc] = True
-            q.put((nr, nc, curr[2] + 1))
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < 5 and 0 <= ny < 5:
+                if visited[nx][ny]:
+                    continue
+                if place[nx][ny] =='X':
+                    continue
+                if place[nx][ny] == 'P':
+                    return False
+                if place[nx][ny] == 'O':
+                    q.append((nx, ny, dist + 1))
+                    visited[nx][ny] = True
     return True
 
 def check(place):
@@ -35,11 +34,11 @@ def check(place):
             if place[r][c] == 'P':
                 if bfs(place, r, c) == False:
                     return False
-    return True
+    return True        
 
 def solution(places):
     answer = []
-    
+
     for place in places:
         if check(place):
             answer.append(1)

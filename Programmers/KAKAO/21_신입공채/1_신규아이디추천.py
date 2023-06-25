653baaa5ebc869f19a2c70b4354c24af7d98cc9b
min <smh92333@gmail.com>
2023-06-25 23:52:35 +0900

    230625

--- Programmers/KAKAO/21_신입공채/1_신규아이디추천.py
+++ Programmers/KAKAO/21_신입공채/1_신규아이디추천.py
@@ -1,1 +1,40 @@
-.
+def isValid(ch):
+    if ch.isalnum():
+        return True
+    if ch == '-' or ch == '_' or ch == '.':
+        return True
+    return False
+
+def solution(new_id):
+    answer = ''
+
+    lastDot = False
+    for ch in new_id:
+        if isValid(ch) == False:  # 2단계
+            continue
+
+        if ch == '.':  # 3,4단계
+            if len(answer) == 0 or lastDot:
+                continue
+            lastDot = True
+        else:
+            lastDot = False
+
+        ch = ch.lower()  # 1단계
+        answer += ch
+
+    if len(answer) == 0:  # 5단계
+        answer += 'a'
+
+    if len(answer) >= 16:  # 6단계
+        answer = answer[:15]
+
+    if answer.endswith('.'):  # 6단계
+        answer = answer[:-1]
+
+    if len(answer) <= 2:  # 7단계
+        ch = answer[-1]
+        while len(answer) < 3:
+            answer += ch
+
+    return answer

a4ad112405f2f13d2571807549ca82c46a27a05d
min486 <smh92333@gmail.com>
2024-02-20 19:26:25 +0900

    240220 백준 풀이

---
+++ Baekjoon/DP/10844_쉬운계단수.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+dp = [[0]*12 for _ in range(N+1)]
+dp[1][2:11] = [1]*9
+
+for i in range(2, N+1):
+    for j in range(1, 11):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+
+res = sum(dp[N])
+print(res % 1000000000)
